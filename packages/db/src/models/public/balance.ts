14f34dcd8b96edce5b801343b886705f5813ff69
Brioline <mikelrap@gmail.com>
2021-03-26 09:10:42 +0200

    balance model updated

--- packages/db/src/models/public/balance.ts
+++ packages/db/src/models/public/balance.ts
@@ -4,11 +4,12 @@ import {
   Entity,
   Index,
   JoinColumn,
-  ManyToOne,
+  OneToOne,
   PrimaryGeneratedColumn,
 } from "typeorm";
 import { Field, ID, ObjectType, Int } from "type-graphql";
 import Block from "./block";
+import { Account } from "../index";
 
 @ObjectType()
 @Index("balance_pk", ["balanceId"], { unique: true })
@@ -19,15 +20,27 @@ export default class Balance extends BaseEntity {
   public balanceId: number;
 
   @Field(() => Number)
-  @Column("numeric", { name: "value" })
-  public value: number;
+  @Column("numeric", { name: "free" })
+  public free: number;
+
+  @Field(() => Number)
+  @Column("numeric", { name: "reserved" })
+  public reserved: number;
+
+  @Field(() => Number)
+  @Column("numeric", { name: "misc_frozen" })
+  public miscFrozen: number;
+
+  @Field(() => Number)
+  @Column("numeric", { name: "fee_frozen" })
+  public feeFrozen: number;
 
   @Field(() => Int)
-  @Column("integer", { name: "block_id" })
-  public blockId: number;
+  @Column("integer", { name: "account_id" })
+  public accountId: number;
 
-  @Field(() => Block)
-  @ManyToOne(() => Block, (block) => block.balances)
-  @JoinColumn([{ name: "block_id", referencedColumnName: "blockId" }])
-  public block: Block;
+  @Field(() => Account)
+  @OneToOne(() => Account, (account) => account.balance)
+  @JoinColumn([{ name: "account_id", referencedColumnName: "accountId" }])
+  public account: Block;
 }
