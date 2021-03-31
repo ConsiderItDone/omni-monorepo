7679f9e0afec70b0e60282a44d4f4f5375d0e32d
Brioline <mikelrap@gmail.com>
2021-03-31 13:34:03 +0300

    backfiller pogress perPage & defaults added

--- packages/db/src/models/public/backfillProgress.ts
+++ packages/db/src/models/public/backfillProgress.ts
@@ -1,15 +1,25 @@
 import { Field, ID, ObjectType } from "type-graphql";
-import { Index, Column, Entity, BaseEntity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Index,
+  Column,
+  Entity,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 
 @ObjectType()
 @Index("backfill_progress_pkey", ["backfillProgressId"], { unique: true })
 @Entity("backfill_progress", { schema: "public" })
 export default class BackfillProgress extends BaseEntity {
-  @Field(()=> ID)
+  @Field(() => ID)
   @PrimaryGeneratedColumn({ type: "integer", name: "backfill_progress_id" })
   public backfillProgressId: number;
 
-  @Field(()=> String)
+  @Field(() => String)
   @Column("bigint", { name: "last_block_number" })
   public lastBlockNumber: string;
+
+  @Field(() => Number)
+  @Column("integer", { name: "per_page" })
+  public perPage: number;
 }
--- packages/db/src/repositories/public/backfillProgressRepository.ts
+++ packages/db/src/repositories/public/backfillProgressRepository.ts
@@ -1,10 +1,15 @@
 import { EntityRepository, Repository, UpdateResult } from "typeorm";
 import BackfillProgress from "../../models/public/backfillProgress";
 
+const progressDefaults = {
+  backfillProgressId: 1,
+  lastBlockNumber: "0",
+  perPage: 1000,
+};
 @EntityRepository(BackfillProgress)
 export default class BackfillProgressRepository extends Repository<BackfillProgress> {
   public async init() {
-    return this.save({ backfillProgressId: 1, lastBlockNumber: "0" });
+    return this.save(progressDefaults);
   }
   private async getProgress(): Promise<BackfillProgress> {
     return (await this.findOne(1)) || (await this.init());
