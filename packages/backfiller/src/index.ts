a61bca254a661e9bbee3d28b8d97068f47fc8c6f
ramil <ramilexe@gmail.com>
2021-05-06 14:10:46 +0300

    fix linter errors

--- packages/backfiller/src/index.ts
+++ packages/backfiller/src/index.ts
@@ -1,7 +1,7 @@
 import * as dotenv from "dotenv";
 import path from "path";
 try {
-  dotenv.config({ path: path.resolve(__dirname) + "/../../../.env"});
+  dotenv.config({ path: path.resolve(__dirname) + "/../../../.env" });
 } catch (e) {
   //nop
 }
--- packages/indexer/src/index.ts
+++ packages/indexer/src/index.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 import path from "path";
 
 try {
-  dotenv.config({ path: path.resolve(__dirname) + "/../../../.env"});
+  dotenv.config({ path: path.resolve(__dirname) + "/../../../.env" });
 } catch (e) {
   //nop
 }
