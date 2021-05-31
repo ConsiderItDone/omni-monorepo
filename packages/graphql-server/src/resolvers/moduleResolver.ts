b9abf38d286af232bf807e7d763442de7d00d804
Mikhail Reenko <reenko@yandex.ru>
2021-05-31 17:59:37 +0700

    NOD-142 Add types into module

--- packages/graphql-server/src/resolvers/moduleResolver.ts
+++ packages/graphql-server/src/resolvers/moduleResolver.ts
@@ -1,8 +1,15 @@
-import { Resolver } from "type-graphql";
+import { FieldResolver, Resolver, Root } from "type-graphql";
 import Module from "@nodle/db/src/models/public/module";
 import { createBaseResolver } from "../baseResolver";
+import { arrayFieldResolver } from "../fieldsResolver";
+import EventType from "@nodle/db/src/models/public/eventType";
 
 const ModuleBaseResolver = createBaseResolver("Module", Module);
 
 @Resolver(Module)
-export default class ModuleResolver extends ModuleBaseResolver {}
+export default class ModuleResolver extends ModuleBaseResolver {
+  @FieldResolver()
+  eventTypes(@Root() source: Module): Promise<EventType[]> {
+    return arrayFieldResolver(source, EventType, "moduleId");
+  }
+}
