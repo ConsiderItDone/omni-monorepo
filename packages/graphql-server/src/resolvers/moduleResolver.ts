83f5de37b3ce2e167b06aa8733785e0976113331
Mikhail Reenko <reenko@yandex.ru>
2021-06-01 12:52:17 +0700

    NOD-160 Add extrinsic types (FK: module)

--- packages/graphql-server/src/resolvers/moduleResolver.ts
+++ packages/graphql-server/src/resolvers/moduleResolver.ts
@@ -3,6 +3,7 @@ import Module from "@nodle/db/src/models/public/module";
 import { createBaseResolver } from "../baseResolver";
 import { arrayFieldResolver } from "../fieldsResolver";
 import EventType from "@nodle/db/src/models/public/eventType";
+import ExtrinsicType from "@nodle/db/src/models/public/extrinsicType";
 
 const ModuleBaseResolver = createBaseResolver("Module", Module);
 
@@ -11,5 +12,10 @@ export default class ModuleResolver extends ModuleBaseResolver {
   @FieldResolver()
   eventTypes(@Root() source: Module): Promise<EventType[]> {
     return arrayFieldResolver(source, EventType, "moduleId");
+  }
+
+  @FieldResolver()
+  extrinsicTypes(@Root() source: Module): Promise<ExtrinsicType[]> {
+    return arrayFieldResolver(source, ExtrinsicType, "moduleId");
   }
 }
