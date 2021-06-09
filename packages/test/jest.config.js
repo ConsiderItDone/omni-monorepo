c95d0399f3ae49285539b2c17b96d138ab84a704
Mikhail Reenko <reenko@yandex.ru>
2021-06-09 09:16:44 +0700

    NOD-181 Jest setup

---
+++ packages/test/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testTimeout: 60 * 1000,
+  testMatch: ["**/jest/?(*.)+(spec|test).+(ts|tsx|js)"],
+};
---
+++ packages/test/jest/one.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from "@jest/globals";
+
+describe("Preparation", () => {
+  it("1 + 3 should be 4", async () => {
+    const res = 1 + 3;
+    expect(res).toBe(4);
+  });
+});
