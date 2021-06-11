7dd70a0427c8450dd27abf8c912125b9920dbe76
Mikhail Reenko <reenko@yandex.ru>
2021-06-11 16:45:30 +0700

    Fix migration (#37)

--- packages/db/src/migrations/1623125843733-UpdateModuleFunctions.ts
+++ packages/db/src/migrations/1623125843733-UpdateModuleFunctions.ts
@@ -1,11 +1,8 @@
 import {getConnection, MigrationInterface, QueryRunner} from "typeorm";
-import ExtrinsicTypeRepository from "../repositories/public/extrinsicTypeRepository";
 
 export class UpdateModuleFunctions1623125843733 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        const extrinsicTypeRepository = getConnection().getCustomRepository(ExtrinsicTypeRepository);
-
         const modules = await queryRunner.query(`SELECT module_id from "public"."module"`);
         for (const module of modules) {
             const items = await queryRunner.query(`
@@ -16,11 +13,17 @@ export class UpdateModuleFunctions1623125843733 implements MigrationInterface {
             `);
 
             for (const obj of items) {
-                const extType = await extrinsicTypeRepository.addOrIgnore({
-                    name: obj.call_module_function,
-                    moduleId: module.module_id,
-                });
-                await queryRunner.query(`UPDATE "public"."extrinsic" SET extrinsic_type_id=${extType.extrinsicTypeId} WHERE call_module_function='${items.call_module_function} and module_id=${module.module_id}'`);
+                let id;
+                const extrinsicType = await queryRunner.query(`SELECT extrinsic_type_id from "public"."extrinsic_type" WHERE name='${obj.call_module_function}' and module_id=${module.module_id}`);
+
+                if (extrinsicType && extrinsicType.length && extrinsicType[0].extrinsic_type_id) {
+                    id = extrinsicType[0].extrinsic_type_id;
+                } else {
+                    const newExtrinsicType = await queryRunner.query(`INSERT INTO "public"."extrinsic_type"("name", "module_id") VALUES ('${obj.call_module_function}', ${module.module_id}) RETURNING extrinsic_type_id;`);
+                    id = newExtrinsicType[0].extrinsic_type_id
+                }
+
+                await queryRunner.query(`UPDATE "public"."extrinsic" SET extrinsic_type_id=${id} WHERE call_module_function='${items.call_module_function} and module_id=${module.module_id}'`);
             }
         }
 
