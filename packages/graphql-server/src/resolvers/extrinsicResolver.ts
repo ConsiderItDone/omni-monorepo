5438298e72eb1af3e4d1bba783466f807bc58a90
Mike <mikelrap@gmail.com>
2021-06-21 10:02:33 +0300

    fix acc id

--- packages/graphql-server/src/resolvers/extrinsicResolver.ts
+++ packages/graphql-server/src/resolvers/extrinsicResolver.ts
@@ -114,8 +114,8 @@ export default class ExtrinsicResolver extends ExtrinsicBaseResolver {
     }
 
     if (signer) {
-      const signerAcc = await Account.findOne({ where: { address: signer } });
-      findOptions.where = Object.assign(findOptions.where || {}, { signerId: signerAcc.accountId });
+      const { accountId } = await Account.findOne({ where: { address: signer } });
+      findOptions.where = Object.assign(findOptions.where || {}, { signerId: accountId });
     }
 
     let module: Module;
