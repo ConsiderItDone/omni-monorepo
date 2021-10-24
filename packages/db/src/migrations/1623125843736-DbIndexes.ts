7943aea8861ba42d36f7a011734ec989fa55b0b6
ramil <ramilexe@gmail.com>
2021-10-25 02:53:31 +0300

    fix migration

--- packages/db/src/migrations/1623125843736-DbIndexes.ts
+++ packages/db/src/migrations/1623125843736-DbIndexes.ts
@@ -4,10 +4,10 @@ export class DbIndexes1623125843736 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         const sql = `
-            create index extrinsic_block_id_index
+            create index if not exists extrinsic_block_id_index
                 on extrinsic (block_id);
 
-            create index extrinsic_hash_index
+            create index if not exists extrinsic_hash_index
                 on extrinsic (hash);
         `;
 
