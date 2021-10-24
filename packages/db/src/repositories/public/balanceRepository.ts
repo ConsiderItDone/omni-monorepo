34ce20955997b693cf638ab16150b5c15f76aa54
ramil <ramilexe@gmail.com>
2021-10-24 03:54:20 +0300

    optimize event and balance resolvers

--- packages/db/src/repositories/public/balanceRepository.ts
+++ packages/db/src/repositories/public/balanceRepository.ts
@@ -29,4 +29,27 @@ export default class BalanceRepository extends Repository<Balance> {
       ...balanceData,
     });
   }
+  
+  public async getBalanceByAddress(address: string) {
+    const result = await this.query(`
+        SELECT "balance"."balance_id"    AS "balanceId",
+               "balance"."free"          AS "free",
+               "balance"."reserved"      AS "reserved",
+               "balance"."misc_frozen"   AS "miscFrozen",
+               "balance"."fee_frozen"    AS "feeFrozen",
+               "balance"."account_id"    AS "accountId",
+               "balance"."block_id"      AS "blockId"
+        FROM "public"."balance" "balance"
+                 INNER JOIN "public"."account" "account" ON "account"."account_id" = "balance"."account_id"
+                 INNER JOIN "public"."block" "block" ON "block"."block_id" = "balance"."block_id"
+        WHERE "account"."address" = $1
+        ORDER BY "block"."number" DESC 
+        LIMIT 1`, [ address ]);
+    
+    if (!result) {
+      return null;
+    }
+
+    return this.create(result[0]);
+  }
 }
