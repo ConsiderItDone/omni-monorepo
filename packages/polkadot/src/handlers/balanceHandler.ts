9e76bc7ab8921373f2394b3e19804800826597cc
Mike <mikelrap@gmail.com>
2021-12-15 11:01:21 +0200

    balance update

--- packages/polkadot/src/handlers/balanceHandler.ts
+++ packages/polkadot/src/handlers/balanceHandler.ts
@@ -3,15 +3,13 @@ import { EntityManager } from "typeorm";
 import type { AccountId, BlockNumber } from "@polkadot/types/interfaces/runtime";
 import type { Event } from "@polkadot/types/interfaces/system";
 import type { BlockHash } from "@polkadot/types/interfaces/chain";
-import { AccountInfo } from "@polkadot/types/interfaces/system";
-
-import { saveAccount, tryFetchAccount } from "../misc";
-
 import { GenericAccountId } from "@polkadot/types";
-import { logger, LOGGER_ERROR_CONST } from "@nodle/utils/src/logger";
-import { Account, Balance, Block } from "../../../db/src/models";
+
+
 import AccountRepository from "@nodle/db/src/repositories/public/accountRepository";
-import BalanceRepository from "@nodle/db/src/repositories/public/balanceRepository";
+import { logger, LOGGER_ERROR_CONST } from "@nodle/utils/src/logger";
+import { Account, Balance } from "../../../db/src/models";
+import { saveAccount, tryFetchAccount } from "../misc";
 
 export async function handleBalance(
   manager: EntityManager,
@@ -55,35 +53,20 @@ export async function handleBalance(
     logger.error(error);
   }
   async function handleAccountBalance(address: AccountId | string) {
-    console.log("????before repos");
     const accountRepository = manager.getCustomRepository(AccountRepository);
-    const balanceRepostitory = manager.getCustomRepository(BalanceRepository);
 
-    console.log("????After repos");
-
-    logger.info("before");
     const savedAccount = await accountRepository.findOne({ where: { address: address.toString() } });
-    console.log("after");
+
     if (savedAccount) {
-      console.log("!!!!!!!!!!!!!!!!!!!! acc", savedAccount);
-      /*       const savedBalance = await balanceRepostitory.findOne({
-        where: { accountId: savedAccount.accountId },
-        order: { blockId: "DESC" },
-      }); */
       const savedBalance = await Balance.createQueryBuilder("balance")
-        .innerJoin(Block, "block", "block.blockId = balance.blockId")
+        .leftJoinAndSelect("balance.block", "block", "block.blockId = balance.blockId")
         .where(`balance.accountId =:accountId`, { accountId: savedAccount.accountId })
-        .orderBy("block.number", "DESC")
-        .limit(1)
+        .orderBy("block.number", "DESC", "NULLS LAST")
         .getOne();
-      console.log("!!!!!!!!!!!!!!!!!!!! savedBalance", savedBalance);
-      console.log('newCOn')
 
       if (savedBalance) {
-        console.log('parseInt')
-        const isOldBalance = Number(savedBalance.block.number) < blockNumber.toNumber();
+        const isOldBalance = Number(savedBalance?.block?.number) < blockNumber.toNumber();
         if (isOldBalance) {
-          console.log("isOld");
           return await saveAccountBalance({ accountId: savedAccount.accountId, balanceId: savedBalance.balanceId });
         }
         return { savedAccount, savedBalance };
@@ -93,7 +76,6 @@ export async function handleBalance(
     return await saveAccountBalance();
 
     async function saveAccountBalance(options?: { accountId?: number; balanceId?: number }) {
-      console.log('options', options)
       const account = await tryFetchAccount(api, address, blockHash, blockNumber);
       return await saveAccount(manager, account, blockId, options);
     }
