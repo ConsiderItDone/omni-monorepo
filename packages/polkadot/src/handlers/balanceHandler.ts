dc1ebc562cc244ee88e633034d0b438dde8e070e
Brioline <33597141+Brioline@users.noreply.github.com>
2021-12-20 04:29:10 +0200

    balance replace (#71)
    * balance replace
    
    * balance update
    
    * prettier + lint
    
    * add limit to balance select query
    
    * prettier
    
    Co-authored-by: ramil <ramilexe@gmail.com>

--- packages/polkadot/src/handlers/balanceHandler.ts
+++ packages/polkadot/src/handlers/balanceHandler.ts
@@ -1,15 +1,14 @@
 import { ApiPromise } from "@polkadot/api";
 import { EntityManager } from "typeorm";
-import type { BlockNumber } from "@polkadot/types/interfaces/runtime";
+import type { AccountId, BlockNumber } from "@polkadot/types/interfaces/runtime";
 import type { Event } from "@polkadot/types/interfaces/system";
 import type { BlockHash } from "@polkadot/types/interfaces/chain";
-import { AccountInfo } from "@polkadot/types/interfaces/system";
-
-import { saveAccount, tryFetchAccount } from "../misc";
-
 import { GenericAccountId } from "@polkadot/types";
+
+import AccountRepository from "@nodle/db/src/repositories/public/accountRepository";
 import { logger, LOGGER_ERROR_CONST } from "@nodle/utils/src/logger";
 import { Account, Balance } from "../../../db/src/models";
+import { saveAccount, tryFetchAccount } from "../misc";
 
 export async function handleBalance(
   manager: EntityManager,
@@ -22,27 +21,9 @@ export async function handleBalance(
   try {
     switch (event.method) {
       case "Transfer": {
-        const accFrom = [
-          event.data[0],
-          await tryFetchAccount(api, event.data[0] as GenericAccountId, blockHash, blockNumber),
-        ];
-        const accTo = [
-          event.data[1],
-          await tryFetchAccount(api, event.data[1] as GenericAccountId, blockHash, blockNumber),
-        ];
         try {
-          const savedAccountBalanceFrom = await saveAccount(
-            manager,
-            accFrom[0] as GenericAccountId,
-            accFrom[1] as AccountInfo,
-            blockId
-          );
-          const savedAccountBalanceTo = await saveAccount(
-            manager,
-            accTo[0] as GenericAccountId,
-            accTo[1] as AccountInfo,
-            blockId
-          );
+          const savedAccountBalanceFrom = await handleAccountBalance(event.data[0] as GenericAccountId);
+          const savedAccountBalanceTo = await handleAccountBalance(event.data[1] as GenericAccountId);
           return [savedAccountBalanceFrom, savedAccountBalanceTo];
         } catch (accountSaveError) {
           logger.error(LOGGER_ERROR_CONST.ACCOUNT_SAVE_ERROR(blockNumber.toNumber()), accountSaveError);
@@ -52,17 +33,8 @@ export async function handleBalance(
       case "DustLost":
       case "Unreserved":
       case "Reserved": {
-        const acc = [
-          event.data[0],
-          await tryFetchAccount(api, event.data[1] as GenericAccountId, blockHash, blockNumber),
-        ];
         try {
-          const savedAccountBalance = await saveAccount(
-            manager,
-            acc[0] as GenericAccountId,
-            acc[1] as AccountInfo,
-            blockId
-          );
+          const savedAccountBalance = await handleAccountBalance(event.data[1] as GenericAccountId);
           return [savedAccountBalance];
         } catch (accountSaveError) {
           logger.error(LOGGER_ERROR_CONST.ACCOUNT_SAVE_ERROR(blockNumber.toNumber()), accountSaveError);
@@ -74,5 +46,34 @@ export async function handleBalance(
     }
   } catch (error) {
     logger.error(error);
+  }
+  async function handleAccountBalance(address: AccountId | string) {
+    const accountRepository = manager.getCustomRepository(AccountRepository);
+
+    const savedAccount = await accountRepository.findOne({ where: { address: address.toString() } });
+
+    if (savedAccount) {
+      const savedBalance = await Balance.createQueryBuilder("balance")
+        .innerJoinAndSelect("balance.block", "block")
+        .where(`balance.accountId =:accountId`, { accountId: savedAccount.accountId })
+        .orderBy("block.number", "DESC", "NULLS LAST")
+        .limit(1)
+        .getOne();
+
+      if (savedBalance) {
+        const isOldBalance = Number(savedBalance?.block?.number) < blockNumber.toNumber();
+        if (isOldBalance) {
+          return await saveAccountBalance({ accountId: savedAccount.accountId, balanceId: savedBalance.balanceId });
+        }
+        return { savedAccount, savedBalance };
+      }
+      return await saveAccountBalance({ accountId: savedAccount.accountId });
+    }
+    return await saveAccountBalance();
+
+    async function saveAccountBalance(options?: { accountId?: number; balanceId?: number }) {
+      const account = await tryFetchAccount(api, address, blockHash, blockNumber);
+      return await saveAccount(manager, account, blockId, options);
+    }
   }
 }
