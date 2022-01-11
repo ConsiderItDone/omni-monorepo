e4f4e26a1ca38961f61ce291b7a022421ea7f0c4
Mike <mikelrap@gmail.com>
2022-01-11 11:01:16 +0200

    variables

--- packages/pagerduty/src/index.ts
+++ packages/pagerduty/src/index.ts
@@ -1,6 +1,39 @@
 import Client from "./client";
 import { BLOCK } from "./queries/index";
+import * as queries from "./queries";
+
 console.log("hello");
 const client = new Client(process.env.GRAPHQL_ENDPOINT);
 
-client.query(BLOCK, { id: "123456" });
+const variables: { [key: string]: any } = {
+  /*   BLOCKS
+BLOCK
+TRANSFERS
+CHART_TRANSFERTS
+CHART_EXTRINSICS
+ACCOUNTS
+VALIDATORS
+EXTRINSICS
+EXTRINSICSDETAILTS
+EVENTS
+SEARCH
+ROOT_CERTIFICATES
+APPLICATIONS
+ACCOUNTBYADDRESS
+EXTRINSIC_FILTER_OPTIONS
+EVENT_FILTER_OPTIONS
+ALLOCATIONS
+TRANSFERDETAILS */
+};
+
+function run() {
+  const keys = Object.keys(queries);
+  for (const key of keys) {
+    if (variables[key]) {
+      //@ts-ignore
+      client.query(queries[key], variables[key]);
+    }
+  }
+}
+
+run();
