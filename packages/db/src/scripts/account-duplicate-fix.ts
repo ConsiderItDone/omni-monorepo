1d1b3a10a6ddb8bb7b37e143791f786f585b2dd8
Mike <mikelrap@gmail.com>
2022-01-12 09:03:24 +0200

    quey changed

--- packages/db/src/scripts/account-duplicate-fix.ts
+++ packages/db/src/scripts/account-duplicate-fix.ts
@@ -30,26 +30,54 @@ async function start() {
     FROM account
     GROUP BY address
     HAVING count(*) > 1
-    LIMIT 1000;
+    LIMIT 100;
     `;
-    const res = await connection.query(q1);
-    if (!res.length) break;
+    const duplicates = await connection.query(q1);
+    console.log("duplicates lenght", duplicates.length);
+    if (!duplicates.length) break;
     else {
-      for (const acc of res) {
+      for (const acc of duplicates) {
         const query = `
-        WITH deleted_accounts as (
-          DELETE FROM account a USING (
-                 SELECT MIN(account_id) as account_id, address
-                   FROM account
-                   WHERE address = '${acc.address}'
-                   GROUP BY address HAVING COUNT(*) > 1   
-                 ) b
-                 WHERE a.address = b.address 
-                 AND a.account_id <> b.account_id
-             returning a.account_id
-         ) 
-           DELETE FROM balance
-           WHERE account_id IN (SELECT account_id FROM deleted_accounts)`;
+        WITH get_min_account AS (
+          SELECT MIN(account_id) as account_id, address
+          FROM account
+          WHERE address = '${acc.address}'
+          GROUP BY address 
+          HAVING COUNT(*) > 1
+      ),
+       account_for_deletion as (
+           SELECT 
+                  a.account_id as duplicate_id, 
+                  a.address, 
+                  b.account_id as real_id
+           FROM account a
+           INNER JOIN get_min_account b ON (a.address = b.address
+             AND a.account_id != b.account_id)
+      ),
+      update_extrinsic AS (
+          update extrinsic
+              set signer_id=(SELECT account_id FROM get_min_account)
+              where signer_id IN (SELECT duplicate_id as account_id FROM account_for_deletion)
+          RETURNING *
+      ),
+      delete_balance AS (
+          DELETE FROM balance
+          WHERE account_id IN (SELECT duplicate_id as account_id FROM account_for_deletion)
+          RETURNING *
+      ),
+      delete_account AS (
+          DELETE FROM account 
+          where account_id IN (SELECT duplicate_id as account_id FROM account_for_deletion)
+          RETURNING *
+      ),
+      delete_obsolete_balance AS (
+          DELETE FROM balance
+          WHERE account_id = (SELECT account_id FROM get_min_account)
+              AND block_id IS NULL
+          RETURNING *
+      )
+      SELECT * FROM update_extrinsic, delete_balance, delete_account, delete_obsolete_balance;
+`;
         await connection.query(query);
         console.log(acc.address + " duplicates deleted");
       }
@@ -58,20 +86,3 @@ async function start() {
 }
 
 start();
-
-/* 
-const query = `
-WITH deleted_accounts as (
-  DELETE FROM account a USING (
-         SELECT MIN(account_id) as account_id, address
-           FROM account
-           WHERE address = '${acc.address}'
-           GROUP BY address HAVING COUNT(*) > 1   
-         ) b
-         WHERE a.address = b.address 
-         AND a.account_id <> b.account_id
-     returning a.account_id
- ) 
-   DELETE FROM balance
-   WHERE account_id IN (SELECT account_id FROM deleted_accounts)`; 
-   */
