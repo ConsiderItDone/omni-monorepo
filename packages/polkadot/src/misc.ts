10953625b3c4696273808e49bad160fcb1a03f2f
Mike <mikelrap@gmail.com>
2022-01-18 18:10:22 +0200

    balance-value-fix

--- packages/polkadot/src/misc.ts
+++ packages/polkadot/src/misc.ts
@@ -329,10 +329,10 @@ export async function saveAccount(
   const { free, reserved, miscFrozen, feeFrozen } = balance;
   const balanceData = {
     accountId: savedAccount.accountId,
-    free: free.toString(),
-    reserved: reserved.toString(),
-    miscFrozen: miscFrozen.toString(),
-    feeFrozen: feeFrozen.toString(),
+    free: free.toNumber().toString(),
+    reserved: reserved.toNumber().toString(),
+    miscFrozen: miscFrozen.toNumber().toString(),
+    feeFrozen: feeFrozen.toNumber().toString(),
     blockId,
   };
   const savedBalance = await balanceRepository.upsert(options?.balanceId, balanceData);
